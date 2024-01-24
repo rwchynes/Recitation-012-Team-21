00f7731d086a3f07dc8256edce25e97ae6efde60
Ramiro Calvello <ramirocalvello@gmail.com>
2024-01-24 15:45:47 -0700

    Version - Ramiro : 1

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> LessMask(vector<double> nums, double less_than);
+vector<bool> GreaterMask(vector<int> nums, int greater_than);
+int main() { return 1; }
